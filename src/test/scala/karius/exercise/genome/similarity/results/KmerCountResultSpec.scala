23d859048c794b328b94cefeacb314ef944e0097
TangE <ellen.tang@moodys.com>
2019-09-06 21:37:49 -0700

    add unit tests for result handling. - isComparable - merge results

--- src/test/scala/karius/exercise/genome/similarity/results/KmerCountResultSpec.scala
+++ src/test/scala/karius/exercise/genome/similarity/results/KmerCountResultSpec.scala
@@ -17,4 +17,32 @@ class KmerCountResultSpec extends FlatSpec {
     val r2 = KmerCountResult( 2, Map( "AGC" -> 3, "TGG" -> 1 ) )
     assert ( r1.isComparable(r2, 0.8) == false)
   }
+
+  it should "merge with other result of the same genome index" in {
+    val r0 = KmerCountResult( 1, Map.empty[String, Int] )
+    val r1 = KmerCountResult( 1, Map( "AGC" -> 3, "TGG" -> 2 ) )
+    val r2 = KmerCountResult( 1, Map( "AGC" -> 3, "TGG" -> 1 , "ATC"-> 4) )
+
+    val r = r1.combine(r2 )
+    println (r)
+
+    assert (r.get.result.size == 3)
+    assert (r.get.result.getOrElse("AGC", 0) == 6 )
+    assert (r.get.result.getOrElse("TGG", 0) == 3)
+    assert (r.get.result.getOrElse("ATC", 0) == 4 )
+
+
+    val r01 = r0.combine(r1)
+    println(r01)
+    assert (r01.get.result.size == 2)
+    assert (r01.get.result.getOrElse("AGC", 0) == 3 )
+  }
+
+  "Result of different genome index" should "not be combined" in {
+    val r1 = KmerCountResult( 1, Map( "AGC" -> 3, "TGG" -> 2 ) )
+    val r2 = KmerCountResult( 2, Map( "AGC" -> 3, "TGG" -> 1 , "ATC"-> 4) )
+
+    val r = r1.combine(r2 )
+    assert (r.isEmpty)
+  }
 }
